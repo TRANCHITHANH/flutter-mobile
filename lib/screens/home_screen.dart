91b06fd382800ca03654709529a515ed8f31fb76
TRANCHITHANH <tchithanh453@gmail.com>
2026-01-13 22:21:57 +0700

    Ghi chú code *

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
-import 'package:table_calendar/table_calendar.dart';
-import '../providers/todo_provider.dart';
-import '../models/task_model.dart';
+import 'package:intl/intl.dart';//format ngày giờ: DateFormat('HH:mm').format(dateTime)--> 16:01
+import 'package:provider/provider.dart'; //Dùng để nghe và cập nhật dữ liệu từ Provider
+import 'package:table_calendar/table_calendar.dart'; //Thư viện hiển thị lịch
+import '../providers/todo_provider.dart';//Quản lý dữ liệu (task, user), kết nối giữa UI và logic nghiệp vụ
+import '../models/task_model.dart';//File định nghĩa cấu trúc dữ liệu của một Task
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,38 +11,38 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  DateTime _selectedDay = DateTime.now();
-  bool _showCompleted = false;
+class _HomeScreenState extends State<HomeScreen> {//widget có trạng thái thay đổi
+  DateTime _selectedDay = DateTime.now();// Biến lưu ngày người dùng đang chọn trên lịch.
+  bool _showCompleted = false;// Biến điều khiển Tab: false (Việc cần làm), true (Việc đã xong).
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {// xây dựng giao diện
     return Theme(
       data: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black, 
-        primaryColor: const Color(0xFF7E84FF)
+        scaffoldBackgroundColor: Colors.black, // Nền tối
+        primaryColor: const Color(0xFF7E84FF) // màu tím chủ đạo
       ),
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: const Color(0xFF7E84FF),
-          shape: const CircleBorder(),
-          elevation: 4,
-          onPressed: () => _showForm(context, null), // Gọi form thêm mới (null)
-          child: const Icon(Icons.add, color: Colors.white, size: 32),
+      child: Scaffold(//khung màn hình bố cục giao diện
+        floatingActionButton: FloatingActionButton(// nút hành động nổi: thêm mới công việc
+          backgroundColor: const Color(0xFF7E84FF),// màu tím
+          shape: const CircleBorder(),// hình tròn
+          elevation: 4, //độ nổi bóng 
+          onPressed: () => _showForm(context, null), // sự kiện khi bấm nút: hiển thị form thêm mới công việc
+          child: const Icon(Icons.add, color: Colors.white, size: 32), // hiển thị icon dấu cộng, màu trắng, kích thước 32px
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat, // vị trí nút hành động nổi: góc dưới bên phải
 
-        body: SafeArea(
-          child: Column(children: [
+        body: SafeArea(//tự động chừa khoảng trống để nội dung ko bị che
+          child: Column(children: [//sắp xếp các widget con theo cột dọc: Header, Calendar, Tabs, Task List
             Padding(
-              padding: const EdgeInsets.all(15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
+              padding: const EdgeInsets.all(15),//Tạo khoảng cách 15px xung quanh nội dung bên trong
+              child: Row(// bố cục ngang
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,// căn đều 2 bên
+                children: [ //row gồm 2 thành phần: Tên người dùng & nút Logout
                   // Hiển thị tên người dùng lấy từ Provider
                   Consumer<TodoProvider>(builder: (_, prov, __) => Text(
                     "Hi, ${prov.currentUsername}", 
-                    style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)
+                    style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)// chữ màu trắng, cỡ 18, in đậm
                   )),
                   
                   IconButton(
@@ -55,37 +55,49 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
             ),
-            TableCalendar(
-              firstDay: DateTime.utc(2023), lastDay: DateTime.utc(2030),
-              focusedDay: _selectedDay, currentDay: _selectedDay,
-              calendarFormat: CalendarFormat.month,
-              headerStyle: const HeaderStyle(
-                formatButtonVisible: false, titleCentered: true, 
+
+            TableCalendar(// widget hiển thị lịch
+              firstDay: DateTime.utc(2023), lastDay: DateTime.utc(2030),// khoảng ngày hiển thị từ 2023 đến 2030, dùng DateTime.utc để tránh lỗi múi giờ
+              focusedDay: _selectedDay, currentDay: _selectedDay,//_selectedDay là ngày được chọn và cũng là ngày hiện tại
+              calendarFormat: CalendarFormat.month,// định dạng hiển thị theo tháng
+              headerStyle: const HeaderStyle(//header của lịch
+                formatButtonVisible: false, titleCentered: true, // ẩn nút Month/Week, căn giữa tiêu đề
                 titleTextStyle: TextStyle(color: Colors.white, fontSize: 18)
               ),
-              calendarStyle: const CalendarStyle(
-                defaultTextStyle: TextStyle(color: Colors.white),
+
+              calendarStyle: const CalendarStyle(// style của lịch
+                defaultTextStyle: TextStyle(color: Colors.white),// ngày thường màu trắng suốt
+                //ngày đc chọn: nền tím, bo góc vuông tròn nhẹ
                 selectedDecoration: BoxDecoration(color: Color(0xFF7E84FF), shape: BoxShape.rectangle, borderRadius: BorderRadius.all(Radius.circular(10))),
+                //hôm nay: nền xám, hình tròn
                 todayDecoration: BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
               ),
-              onDaySelected: (s, f) => setState(() => _selectedDay = s),
+              onDaySelected: (s, f) => setState(() => _selectedDay = s),// sự kiện khi người dùng chọn ngày trên lịch: cập nhật _selectedDay và làm mới UI
             ),
             const SizedBox(height: 15),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              _tabBtn("Today", !_showCompleted, () => setState(() => _showCompleted = false)),
+
+            //2 nút 1 biến state
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [// căn giữa 2 nút tab
+              _tabBtn("Today", !_showCompleted, () => setState(() => _showCompleted = false)),// nếu: _showCompleted == false --> tab Today hoạt động
               const SizedBox(width: 15),
-              _tabBtn("Completed", _showCompleted, () => setState(() => _showCompleted = true)),
+              _tabBtn("Completed", _showCompleted, () => setState(() => _showCompleted = true)),// ngược lại
             ]),
             const SizedBox(height: 15),
-            Expanded(
+            
+            Expanded(//witget này chiếm toàn bộ không gian còn lại, ListView cuộn mượt hơn
+            // Consumer kết nối trực tiếp TodoProvider, khi nào Provider gọi lệnh notifyListeners (CRUD Task, toggle, v.v.) --> Consumer nhận tín hiệu và làm mới UI bên trong nó (chạy builder)
               child: Consumer<TodoProvider>(builder: (ctx, prov, _) {
+              //lọc 2 điều kiện: 
+              //prov.tasks: danh sách tất cả công việc của người dùng hiện tại
+              //đúng ngày đang chọn trên lịch & đúng trạng thái hoàn thành/chưa hoàn thành
                 final list = prov.tasks.where((t) => isSameDay(t.date, _selectedDay) && t.isCompleted == _showCompleted).toList();
-                return list.isEmpty 
-                  ? const Center(child: Text("Empty", style: TextStyle(color: Colors.grey))) 
-                  : ListView.builder(
-                      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 80),
-                      itemCount: list.length,
-                      itemBuilder: (_, i) => _taskItem(list[i], prov));
+                //.toList(): Gom những việc thỏa mãn điều kiện lại thành một danh sách mới để chuẩn bị hiển thị.
+                return list.isEmpty // xử lý khi danh sách rỗng
+                  ? const Center(child: Text("Empty", style: TextStyle(color: Colors.grey))) // nếu rỗng hiển thị chữ "Empty" màu xám ở giữa
+                  : ListView.builder(//nếu có dữ liệu thì hiển thị danh sách công việc: 5 - 6 dòng 1 lúc, cuộn mượt, tránh tập thể -> lag màn hình
+                      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 80),// Căn lề cho đẹp, tránh bị che bởi nút ở góc
+                      itemCount: list.length, // Báo cho máy biết danh sách có bao nhiêu dòng (để chuẩn bị thanh cuộn)
+                      itemBuilder: (_, i) => _taskItem(list[i], prov));//vẽ từng dòng (tiêu đề, màu sắc, checkbox, v.v.), nó lấy công việc thứ i của List[i] để hiển thị
               }),
             )
           ]),
@@ -94,91 +106,131 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _tabBtn(String txt, bool active, VoidCallback onTap) => GestureDetector(
+  // Widget nút chuyển tab (today / completed)
+  // chữ hiển thị trên nút, trạng thái hoạt động(sáng đèn - tối đèn), sự kiện khi bấm
+  Widget _tabBtn(String txt, bool active, VoidCallback onTap) => GestureDetector( // bộ cảm biến ấn nút chạy hàm dưới, GestureDetector: vô hình
     onTap: onTap,
-    child: Container(
+    child: Container(// vô hình -> hình dạng hiển thị nút bt phản hồi
+      //nó đẩy chữ text cách lề trên & dưới 10 px, cách lề trái & phải 30 px
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+      //1: xử lý màu viền: active T: tô tím, F: nền trong suốt
+      //2: bo tròn góc
+      //3: xử lý viền, active F: thấy khung viền xám
       decoration: BoxDecoration(color: active ? const Color(0xFF7E84FF) : Colors.transparent, borderRadius: BorderRadius.circular(10), border: active ? null : Border.all(color: Colors.grey)),
+      //đặt dòng chữ vào giữa ô, chữ luôn màu trắng, in đậm
       child: Text(txt, style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
     ),
   );
 
-  Widget _taskItem(Task task, TodoProvider prov) => Dismissible(
-    key: Key(task.id),
-    onDismissed: (_) => prov.delete(task.id),
+  Widget _taskItem(Task task, TodoProvider prov) => Dismissible(//Dismissible: cơ chế vuốt để xoá
+    key: Key(task.id),//key định danh công việc
+    onDismissed: (_) => prov.delete(task.id),// khi vuốt hết hành trình hàm onDismissed như kèo súng gọi hàm prov.delete xoá viễn viễn dữ liệu DB & RAM
+    //hình ảnh chìm dưới thẻ khi kéo sang mới thấy nền đỏ dưới --> báo hiệu kéo sang sử dụng xóa
     background: Container(color: Colors.red, alignment: Alignment.centerRight, padding: const EdgeInsets.only(right: 20), child: const Icon(Icons.delete)),
+    //giao diện hiển thị
     child: Container(
-      margin: const EdgeInsets.only(bottom: 15),
-      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.only(bottom: 15), // Tạo khoảng cách giữa các thẻ
+      padding: const EdgeInsets.all(15), // Tạo độ thoáng bên trong thẻ
       decoration: BoxDecoration(
-        color: const Color(0xFF1F1F1F),
-        borderRadius: BorderRadius.circular(15),
+        color: const Color(0xFF1F1F1F), // nền đen
+        borderRadius: BorderRadius.circular(15), // bo góc
 
         //Thêm viền màu
         border: Border.all(
-          color: _getPriorityColor(task.priority), // Gọi hàm lấy màu
+          color: _getPriorityColor(task.priority), // Gọi hàm lấy màu viền theo độ ưu tiên
           width: 1.5, // Độ dày của viền
         ),
         
       ),
-      child: Row(children: [
-        InkWell(
+      //sắp xếp nội dung bên trong thẻ: nút check, tiêu đề, nhãn ưu tiên
+      child: Row(children: [// bố trí theo chiều ngang
+        InkWell(// biến cái icon vô tri thành nút bấm đc, khi ấn vào thì gọi hàm thay đổi trạng thái toggle(xong rồi chưa xong)
           onTap: () => prov.toggle(task.id), 
           child: Icon(
-            task.isCompleted ? Icons.check_circle : Icons.circle_outlined, 
+            //toán tử 3 ngôi: Nếu xong (true) thì hiện hình tròn đặc có dấu tích (check_circle). Nếu chưa (false) thì hiện vòng tròn rỗng (circle_outlined).
             // Nếu xong rồi thì màu tím, chưa xong thì lấy màu theo độ ưu tiên cho đồng bộ
+            task.isCompleted ? Icons.check_circle : Icons.circle_outlined, 
             color: task.isCompleted ? const Color(0xFF7E84FF) : _getPriorityColor(task.priority)
           )
         ),
-        const SizedBox(width: 15),
-        Expanded(
-          child: InkWell(
+        const SizedBox(width: 15), // các icon & chữ : 15px
+        Expanded(//Expanded ra lệnh tiêu đề quá dài thì có thể xuống dòng, nhãn Category dính sát vào lề phải.
+          child: InkWell(//khi bấm vào đây mở form sửa task
             onTap: () => _showForm(context, task),
+            //sắp xếp dọc: tên công việc ở trên và time ở dưới
             child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              //decoration: ... lineThrough: Nếu xong rồi thì gạch ngang chữ đi (tạo cảm giác hoàn thành).
               Text(task.title, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, decoration: task.isCompleted ? TextDecoration.lineThrough : null)),
+              //Text(DateFormat...): Dùng thư viện intl để định dạng giờ (ví dụ "14:30"). Chữ 12 màu xám.
               Text(DateFormat('HH:mm').format(task.date), style: const TextStyle(color: Colors.grey, fontSize: 12))
             ]),
           ),
         ),
+        //nhãn danh mục
         Container(
+          //nó đẩy chữ text cách lề trên & dưới 4 px, cách lề trái & phải 8 px
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4), 
-          decoration: BoxDecoration(color: Colors.blue.withOpacity(0.2), borderRadius: BorderRadius.circular(5)), 
-          child: Text(task.category, style: const TextStyle(color: Colors.blue, fontSize: 11, fontWeight: FontWeight.bold))
+          decoration: BoxDecoration(color: Colors.blue.withOpacity(0.2), borderRadius: BorderRadius.circular(5)), // nền xanh ,bo góc 
+          child: Text(task.category, style: const TextStyle(color: Colors.blue, fontSize: 11, fontWeight: FontWeight.bold)) // thiết kế chữ: xanh, 11px, in đậm
         )
       ]),
     ),
   );
 
-  //Hàm show from chung add và edit 
-  // Nếu task == null => Là Thêm mới  & >< != null => Là Sửa
-  void _showForm(BuildContext ctx, Task? task) {
+  //Hàm show from chung add và edit (đa năng 2 trong 1 hàm)
+  //Hàm _showForm này chịu trách nhiệm hiển thị một cái hộp thoại (Dialog) để nhập liệu: 
+  // Nếu task == null => Là Thêm mới  & >< != null => Là Sửa (Tạo mới (Add): Nếu không truyền task vào >< Chỉnh sửa (Edit): Nếu có truyền task vào.)
+  void _showForm(BuildContext ctx, Task? task) { // task? nghĩa là có thể có hoặc có thể null
+
+    // * Toán tử ??: Có nghĩa là "Nếu cái đằng trước bị null thì lấy cái đằng sau".
+    // 1. Ô nhập tên: Nếu là sửa (task có dữ liệu) thì điền tên cũ vào. Nếu là mới (task null) thì để rỗng ("").
     final txt = TextEditingController(text: task?.title ?? "");
+    // 2. Độ ưu tiên: Nếu sửa thì lấy cái cũ, nếu mới thì mặc định là 'Medium','Work'
     String pri = task?.priority ?? 'Medium';
     String cat = task?.category ?? 'Work';
+    // 3. Giờ giấc: Nếu sửa thì lấy giờ của task đó, nếu mới thì lấy giờ hiện tại (TimeOfDay.now()).
     TimeOfDay time = task != null ? TimeOfDay.fromDateTime(task.date) : TimeOfDay.now();
 
+    //Dựng khung Dialog & Xử lý trạng thái cục bộ:
+    //showDialog: Lệnh trong Flutter để hiện popup đè lên màn hình.
+    //StatefulBuilder cung cấp một hàm setS (giống setState) nhưng chỉ dành riêng cho cái Dialog này. 
+    //Khi dùng setS, chỉ cái Dialog được vẽ lại, màn hình chính bên dưới giữ nguyên.
     showDialog(context: ctx, builder: (_) => StatefulBuilder(builder: (ctx, setS) => AlertDialog(
-      backgroundColor: const Color(0xFF1F1F1F),
+      backgroundColor: const Color(0xFF1F1F1F),// nền đen
+      // Tiêu đề đổi linh hoạt: "New Task" hoặc "Edit Task" tùy vào đang làm gì
       title: Text(task == null ? "New Task" : "Edit Task", style: const TextStyle(color: Colors.white)),
       content: Column(mainAxisSize: MainAxisSize.min, children: [
+        //ô nhập tên task
         TextField(controller: txt, style: const TextStyle(color: Colors.white), decoration: const InputDecoration(hintText: "Task Name", hintStyle: TextStyle(color: Colors.grey))),
         const SizedBox(height: 10),
+        //Dropdown chọn Danh mục (Work/Sport),                            Khi chọn cái mới -> gọi setS để vẽ lại Dropdown ngay lập tức
         DropdownButton<String>(value: cat, dropdownColor: const Color(0xFF333333), items: ['Work', 'Sport'].map((e) => DropdownMenuItem(value: e, child: Text(e, style: const TextStyle(color: Colors.white)))).toList(), onChanged: (v) => setS(() => cat = v!)),
         const SizedBox(height: 10),
+        //Dropdown chọn Độ ưu tiên (High/Medium/Low),                   Khi chọn cái mới -> gọi setS để vẽ lại Dropdown ngay lập tức
         DropdownButton<String>(value: pri, dropdownColor: const Color(0xFF333333), items: ['High', 'Medium', 'Low'].map((e) => DropdownMenuItem(value: e, child: Text(e, style: const TextStyle(color: Colors.white)))).toList(), onChanged: (v) => setS(() => pri = v!)),
-        TextButton(onPressed: () async { final t = await showTimePicker(context: ctx, initialTime: time); if(t!=null) setS(()=> time=t); }, child: Text("Time: ${time.format(ctx)}"))
+        // nút chọn time
+        TextButton(onPressed: () async { 
+          // Hiện cái đồng hồ xoay xoay để chọn giờ
+          final t = await showTimePicker(context: ctx, initialTime: time);
+          // Nếu người dùng chọn xong (khác null) -> Cập nhật biến time và vẽ lại nút
+          if(t!=null) setS(()=> time=t); }, 
+          child: Text("Time: ${time.format(ctx)}"))// Hiện giờ đã chọn lên nút
       ]),
+      // Nút lặp: "Add" hoặc "Save" tùy vào đang làm gì
       actions: [
         ElevatedButton(style: ElevatedButton.styleFrom(backgroundColor: const Color(0xFF7E84FF)), onPressed: () {
+          // Kiểm tra: Không được để tên trống
           if (txt.text.isNotEmpty) {
+            // Gọi ông quản lý kho (Provider) ra
             final provider = Provider.of<TodoProvider>(ctx, listen: false);
+            // ghép ngày đang chọn ở màn hình và giờ vừa chọn trong dialog
             final newDate = DateTime(_selectedDay.year, _selectedDay.month, _selectedDay.day, time.hour, time.minute);
-            
+            //TH1: tạo mới, quăng dữ liệu vào , Provider tự sinh ID
             if (task == null) {
               provider.addTask(txt.text, pri, newDate, cat);
-            } else {
+            } else {// TH2: sửa task
               final updatedTask = Task(
-                id: task.id, // Giữ nguyên ID
+                id: task.id, // tạo 1 đối tượng task mới nhưng giữ nguyên ID
                 title: txt.text,
                 priority: pri,
                 date: newDate,
@@ -187,7 +239,7 @@ class _HomeScreenState extends State<HomeScreen> {
               );
               provider.updateTask(updatedTask);
             }
-            Navigator.pop(ctx);
+            Navigator.pop(ctx);// Đóng Dialog lại
           }
         }, child: Text(task == null ? "Add" : "Save", style: const TextStyle(color: Colors.white)))
       ],
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,42 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';// quản lý dữ liệu (task, user), kết nối giữa UI và logic nghiệp vụ
 import '../providers/todo_provider.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatefulWidget {// widget đăng nhập có state thay đổi khi người dùng nhập dữ liệu
+  //ko thay đổi cấu trúc nên dùng const 
+  //key: định danh widget trong cây widget (so sánh widget mới - cũ) --> chuyền key lên lớp cha
   const LoginScreen({super.key});
 
   @override
+  //ghi đè: yêu cầu StatefulWidget tạo ra State tương ứng
+  //Method createState: trả về đối tượng quản lý trạng thái
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //lấy dữ liệu người dùng nhập trong TextField
   final _userController = TextEditingController();
   final _passController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
+  Widget build(BuildContext context) {// xây dựng giao diện: cung cấp tt vị trí của widget trong cây UI: dùng gọi Provider, ScanffoldMessenger, Navigator
+    return Scaffold(// khung màn hình chứa bố cục
+      backgroundColor: Colors.black,// nền tối
+
       body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        padding: const EdgeInsets.all(20), //cách viền cách đều 20px
+        child: Column( //sắp xếp theo cột dọc
+          mainAxisAlignment: MainAxisAlignment.center, // canh giữa theo trục chính (dọc)
+
           children: [
-            const Icon(Icons.lock_outline, size: 80, color: Color(0xFF7E84FF)),
-            const SizedBox(height: 20),
-            const Text("Vui lòng đăng nhập", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+            const Icon(Icons.lock_outline, size: 80, color: Color(0xFF7E84FF)), // icon ô khóa
+            const SizedBox(height: 20), //tạo 1 hộp có kích thước cố định
+
+            const Text("Vui lòng đăng nhập", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),//tiêu đề màn hình
             const SizedBox(height: 40),
             
-            TextField(
+            TextField(//gắn textfield với controller khi người dùng nhâp: _userController.text thay đổi
               controller: _userController,
-              style: const TextStyle(color: Colors.white),
+              style: const TextStyle(color: Colors.white),// chữ nhập màu trắng
               decoration: InputDecoration(
-                hintText: "Username", hintStyle: const TextStyle(color: Colors.grey),
-                filled: true, fillColor: const Color(0xFF1F1F1F),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide.none)
+                hintText: "Username", hintStyle: const TextStyle(color: Colors.grey),// chữ gợi ý màu xám
+                filled: true, fillColor: const Color(0xFF1F1F1F), // nền ô nhập
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide.none) //bo góc 10px, ko viền
               ),
             ),
             const SizedBox(height: 15),
+
             TextField(
               controller: _passController,
               obscureText: true, // Ẩn mật khẩu
@@ -50,30 +59,32 @@ class _LoginScreenState extends State<LoginScreen> {
             const SizedBox(height: 30),
             
             // Nút Đăng Nhập
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: const Color(0xFF7E84FF)),
-                onPressed: () async {
-                  final provider = Provider.of<TodoProvider>(context, listen: false);
-                  bool success = await provider.login(_userController.text, _passController.text);
-                  if (!success) {
+            SizedBox(// đặt kích thước cố định cho nút
+              width: double.infinity,// nút rộng hết khung ngang
+              height: 50, // cao 50px
+              child: ElevatedButton(// nút bấm nổi, có hiệu ứng đổ bóng
+                style: ElevatedButton.styleFrom(backgroundColor: const Color(0xFF7E84FF)),// màu nền nút
+                onPressed: () async {// khi user bấm nút đăng nhập
+                  final provider = Provider.of<TodoProvider>(context, listen: false);// lấy đối tượng TodoProvider từ cây widget ra dùng(gọi hàm và ko update UI)
+                  bool success = await provider.login(_userController.text, _passController.text); // lấy dữ liệu từ 2 ô nhập gửi cho hàm login --> chờ kết quả T,F
+                  if (!success) {// nếu đăng nhập thất bại
+                  // hiển thị thông báo ở dưới cùng màn hình
                     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Sai tài khoản hoặc mật khẩu!")));
                   }
                 },
+                //tạo widget chữ trong nút, trang trí chữ màu trắng, in đậm
                 child: const Text("LOGIN", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
               ),
             ),
             const SizedBox(height: 10),
             
             // Nút Đăng Ký
-            TextButton(
+            TextButton(// nút bấm dạng chữ
               onPressed: () async {
                 final provider = Provider.of<TodoProvider>(context, listen: false);
-                if (_userController.text.isEmpty || _passController.text.isEmpty) {
+                if (_userController.text.isEmpty || _passController.text.isEmpty) { // nếu 1 trong 2 ô trống
                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Vui lòng nhập đầy đủ!")));
-                   return;
+                   return;//dừng lại ko chạy tiếp
                 }
                 bool success = await provider.register(_userController.text, _passController.text);
                 if (success) {
